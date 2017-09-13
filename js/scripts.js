890a0c6e24d75ad1e40d8ff69197d810c06cd67d
James Osborn and Kimberly Bordon <james@>
2017-09-13 15:15:39 -0700

    Finalize survey questions, add jumbotron and container to wrap.

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,14 @@ $(document).ready(function() {
     var citizenNameInput = $("input#citizenName").val();
     var favoriteFoodInput = $("select#favoriteFood").val();
     var favoriteMusicInput = $("input:radio[name=favoriteMusic]:checked").val();
+    var dobInput = $("#born").val();
+    var colorInput = $("#color").val();
 
     $(".citizenName").text(citizenNameInput);
     $(".favoriteFood").text(favoriteFoodInput);
     $(".favoriteMusic").text(favoriteMusicInput);
+    $(".dob").text(dobInput);
+    $(".color").text(colorInput);
 
     $("#confirmation").show();
 
